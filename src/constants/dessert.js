84dd216f10f31886fa52081bedb30a8c855c842a
rosenfence <rosenfence@gmail.com>
2023-01-23 00:30:47 +0900

    Add constant with file convention

---
+++ src/constants/dessert.js
@@ -0,0 +1,15 @@
+const dessert = {
+  // 이모지 : dessert.번호.icon
+  // 한글명 : dessert.번호.name
+
+  1: {
+    icon: '🥐',
+    name: '크로아상',
+  },
+  2: {
+    icon: '🥯',
+    name: '베이글',
+  },
+};
+
+export default dessert;
